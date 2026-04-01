2f48038b13bd6c6895a74e14d3aca1102a39797a
rabbyhasanrzs <rabbyhasanrzs518@gmail.com>
2026-04-01 21:51:02 +0600

    set up React app with Vite and install dependencies

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import productsData from './products.json';
+
+const App = () => {
+     return (
+          );
+};
+export default App;
